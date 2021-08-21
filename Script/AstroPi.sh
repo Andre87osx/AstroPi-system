4077890c8f65b33442dedda36846e7c5a4322b2c
Andrea Aimone <78596979+Andre87osx@users.noreply.github.com>
2021-08-21 22:51:09 +0200

    Update AstroPi.sh
    adding cp louncher and update.sh file #56

--- Script/AstroPi.sh
+++ Script/AstroPi.sh
@@ -77,6 +77,10 @@ SOURCES=/etc/apt/sources.list.d/astroberry.list
 		(($? != 0)) && zenity --error --text="Something went wrong in <b>Updating AstroPi Hotspot.service</b>\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --width=300 --title="AstroPi System $ASTROPI_V" && exit 1
 		echo "$password" | sudo -S cp "$HOME"/.AstroPi-system/Script/AstroPiSystem/autohotspot /usr/bin/autohotspot
 		(($? != 0)) && zenity --error --text="Something went wrong in <b>Updating AstroPi Hotspot script</b>\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --width=300 --title="AstroPi System $ASTROPI_V" && exit 1
+		echo "$password" | sudo -S cp "$HOME"/.AstroPi-system/Script/AstroPiSystem/.Update.sh "$HOME"
+		(($? != 0)) && zenity --error --text="Something went wrong in <b>Updating AstroPi .Update.sh script</b>\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --width=300 --title="AstroPi System $ASTROPI_V" && exit 1
+		echo "$password" | sudo -S cp "$HOME"/.AstroPi-system/Script/AstroPiSystem/AstroPi-system "$HOME"
+		(($? != 0)) && zenity --error --text="Something went wrong in <b>Updating AstroPi Launcher</b>\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --width=300 --title="AstroPi System $ASTROPI_V" && exit 1
 		
 		# =================================================================
 		echo "100"
