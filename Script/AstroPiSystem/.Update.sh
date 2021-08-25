ce8f1fcd76767470dff581b4995ffbde2bddb926
Andrea Aimone <78596979+Andre87osx@users.noreply.github.com>
2021-08-25 18:41:21 +0200

    Update .Update.sh

--- Script/AstroPiSystem/.Update.sh
+++ Script/AstroPiSystem/.Update.sh
@@ -24,57 +24,57 @@ echo "$password" | sudo -S chmod +x "$WorkDir"/Script/*.sh
 # Information window for the waiting time
 zenity --info --title="AstroPi System" --text="I prepare the files to run the latest version of AstroPi System.\n<b>The operation can last a few minutes</b>" --width=300
 
-# =================================================================
+# =========================================================================
 # Check the AstroPi git for update.
 	(
-		# =================================================================
-		echo "5"
-		echo "# Preparing update"
-		sleep 2s
+	# =================================================================
+	echo "5"
+	echo "# Preparing update"
+	sleep 2s
         git -C "$WorkDir" pull
         case $? in
         0)
-        zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
+        	zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
         ;;
         1)
-        echo "$password" | sudo -S rm -rf "$WorkDir"
-        cd "$HOME" || exit
-        git clone https://github.com/Andre87osx/AstroPi-system.git
-        mv $HOME/AstroPi-system $HOME/.AstroPi-system
-        git -C "$WorkDir" pull
-        zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
+		echo "$password" | sudo -S rm -rf "$WorkDir"
+		cd "$HOME" || exit
+		git clone https://github.com/Andre87osx/AstroPi-system.git
+		mv $HOME/AstroPi-system $HOME/.AstroPi-system
+		git -C "$WorkDir" pull
+		zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
         ;;
         -1)
-        echo "$password" | sudo -S rm -rf "$WorkDir"
-        cd "$HOME" || exit
-        git clone https://github.com/Andre87osx/AstroPi-system.git
-        mv $HOME/AstroPi-system $HOME/.AstroPi-system
-        git -C "$WorkDir" pull
-        zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
+		echo "$password" | sudo -S rm -rf "$WorkDir"
+		cd "$HOME" || exit
+		git clone https://github.com/Andre87osx/AstroPi-system.git
+		mv $HOME/AstroPi-system $HOME/.AstroPi-system
+		git -C "$WorkDir" pull
+		zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
         ;;
         esac
 
-		# =================================================================
-		echo "75"
-		echo "# Make sure that the script are executable"
-		sleep 2s
+	# =================================================================
+	echo "75"
+	echo "# Make sure that the script are executable"
+	sleep 2s
         chmod +x $HOME/.AstroPi-system/Script/*.sh
         chmod +x $HOME/.AstroPi-system/Script/AstroPiSystem/*.sh
 			
-		# =================================================================
-		echo "100"
-		echo "# One meore second"
+	# =================================================================
+	echo "100"
+	echo "# One meore second"
         sleep 1s 
         
-		) | zenity --progress \
-		--title="AstroPi System $ASTROPI_V" \
-		--text="AstroPi System $ASTROPI_V" \
-		--percentage=0 \
-		--auto-close \
-		--width=300 \
-		--auto-kill
+	) | zenity --progress \
+	--title="AstroPi System $ASTROPI_V" \
+	--text="AstroPi System $ASTROPI_V" \
+	--percentage=0 \
+	--auto-close \
+	--width=300 \
+	--auto-kill
 
-# =================================================================
+# ========================================================================
 
 # I export the password to the script AstroPi.sh
 export password
