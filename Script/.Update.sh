87eb9dbd396ee9100aa36248ec4ef67c4b5c3177
Andrea Aimone <78596979+Andre87osx@users.noreply.github.com>
2021-09-15 18:16:53 +0200

    start rework update.sh for v1.3
    #78

--- Script/.Update.sh
+++ Script/.Update.sh
@@ -7,83 +7,50 @@
 # /_/    \_\___/\__|_|  \___/|_|   |_|
 ####### AstroPi update system ########
 
-# Sudo password request. 
-password=$(zenity --password  --width=300 --title="AstroPi System")
-WorkDir="$HOME"/.AstroPi-system
+# Bash variables
+#=========================================================================
+AstroPi_v=v1.3 - Beta
+GitDir="$HOME"/.AstroPi-system
 for i in ${!functions[@]}
 do
     ${functions[$i]}
 done
 
-# I make sure that the scripts are executable
-echo "$password" | sudo -S chmod +x "$WorkDir"/Script/*.sh
+# Sudo password request. 
+password=$(zenity --password  --width=300 --title="AstroPi System")
 
-# the script makes sure that the user sudo password is correct
+# I make sure that the scripts are executable
+echo "$password" | sudo -S chmod +x "$GitDir"/Script/*.sh
+
+# The script makes sure that the user sudo password is correct
 (( $? != 0 )) && zenity --error --text="<b>Incorrect user password</b>\n\nError in AstroPi System" --width=300 --title="AstroPi - user password required" && exit 1
 
-# Information window for the waiting time
-zenity --info --title="AstroPi System" --text="I prepare the files to run the latest version of AstroPi System.\n<b>The operation can last a few minutes</b>" --width=300
+# Check internet connection first
+wget -q --spider https://github.com/Andre87osx/AstroPi-system
+if [ $? -eq 0 ]; then
 
-# =========================================================================
 # Check the AstroPi git for update.
-	(
-	# =================================================================
-	echo "5"
-	echo "# Preparing update"
-	sleep 2s
-        git -C "$WorkDir" pull
-        case $? in
-        0)
-        	true
-		#zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
-        ;;
-        1)
-		#echo "$password" | sudo -S rm -rf "$WorkDir"
-		#cd "$HOME" || exit
-		#git clone https://github.com/Andre87osx/AstroPi-system.git
-		#mv $HOME/AstroPi-system $HOME/.AstroPi-system
-		#git -C "$WorkDir" pull
-		# zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
-		exit 1
-        ;;
-        -1)
-		#echo "$password" | sudo -S rm -rf "$WorkDir"
-		#cd "$HOME" || exit
-		#git clone https://github.com/Andre87osx/AstroPi-system.git
-		#mv $HOME/AstroPi-system $HOME/.AstroPi-system
-		#git -C "$WorkDir" pull
-		# zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
-		exit 1
-        ;;
-        esac
+	git -C "$GitDir" pull
+	if [ "$?" -ne 0 ]; then
+		if [ !-d "$GitDir" ]; then
+			cd "$HOME" || exit
+			git clone https://github.com/Andre87osx/AstroPi-system.git
+			mv $HOME/AstroPi-system $HOME/.AstroPi-system
+		else
+			cd "$GitDir"
+			git reset --hard
+		fi
 
-	# =================================================================
-	echo "75"
-	echo "# Make sure that the script are executable"
-	sleep 2s
-        chmod +x "$WorkDir"/Script/*.sh
-        chmod +x "$WorkDir"/AstroPiSystem/*.sh
+	
+        chmod +x "$GitDir"/Script/*.sh
+        chmod +x "$GitDir"/AstroPiSystem/*.sh
 			
-	# =================================================================
-	echo "100"
-	echo "# One meore second"
-        sleep 1s 
-        
-	) | zenity --progress \
-	--title="AstroPi System $ASTROPI_V" \
-	--text="AstroPi System $ASTROPI_V" \
-	--percentage=0 \
-	--auto-close \
-	--width=300 \
-	--auto-kill
 
-# ========================================================================
 
-# I export the password to the script AstroPi.sh
+# Export the variable to AstroPi.sh script
 export password
-"$WorkDir"/Script/AstroPi.sh
 
-# launch AstroPi.sh
-echo "$password" | sudo -S "$WorkDir"/Script/AstroPi.sh
+# Start AstroPi.sh
+"$GitDir"/Script/AstroPi.sh
 (( $? != 0 )) && zenity --error --text="Something went wrong. Contact support at\n<b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --width=300 --title="AstroPi System" && exit 1
 exit 0
