39efdb372664b8d7912e797fcd1b154fbf3acca6
Andre87osx <andrea.aimone@outlook.com>
2021-09-25 20:37:19 +0200

    Revert "start rework for v1.3 (#79)"
    This reverts commit f175a0260fabf54fca6dc7a9260cec6273aa852e.

--- Script/.Update.sh
+++ Script/.Update.sh
@@ -7,75 +7,83 @@
 # /_/    \_\___/\__|_|  \___/|_|   |_|
 ####### AstroPi update system ########
 
-# Bash variables
-#=========================================================================
-AstroPi_v=1.3-Beta
-KStars_v=3.5.5v1.3-Beta
-Indi_v=1.9.2
-
-GitDir="$HOME"/.AstroPi-system
-WorkDir="$HOME"/.Projects
-# Define the height in px of the top system-bar:
-TOPMARGIN=27
-# Sum in px of all horizontal borders:
-RIGHTMARGIN=10
-
-# Get width of screen and height of screen
-SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
-SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
-
-# New width and height
-W=$(( SCREEN_WIDTH / 2 - RIGHTMARGIN ))
-H=$(( SCREEN_HEIGHT - 2 * TOPMARGIN ))
-Wprogress=$(( SCREEN_WIDTH / 5 - RIGHTMARGIN ))
-
-#=========================================================================
-
-# Sudo password request.
-password=$(zenity --password  --width=$W --title="AstroPi System")
+# Sudo password request. 
+password=$(zenity --password  --width=300 --title="AstroPi System")
+WorkDir="$HOME"/.AstroPi-system
+for i in ${!functions[@]}
+do
+    ${functions[$i]}
+done
 
 # I make sure that the scripts are executable
-echo "$password" | sudo -S chmod +x "$GitDir"/Script/*.sh
+echo "$password" | sudo -S chmod +x "$WorkDir"/Script/*.sh
 
-# The script makes sure that the user sudo password is correct
-(( $? != 0 )) && zenity --error --text="<b>Incorrect user password</b>\n\nError in AstroPi System" --width=$W --title="AstroPi - user password required" && exit 1
+# the script makes sure that the user sudo password is correct
+(( $? != 0 )) && zenity --error --text="<b>Incorrect user password</b>\n\nError in AstroPi System" --width=300 --title="AstroPi - user password required" && exit 1
 
-(
-echo "# Check internet connection first"
-if wget -q --spider https://github.com/Andre87osx/AstroPi-system; then
+# Information window for the waiting time
+zenity --info --title="AstroPi System" --text="I prepare the files to run the latest version of AstroPi System.\n<b>The operation can last a few minutes</b>" --width=300
 
-echo "# Check if GIT dir exist"
-	if [ ! -d "$GitDir" ]; then
-		cd "$HOME" || exit 1
-		git clone https://github.com/Andre87osx/AstroPi-system.git
-		mv "$HOME"/AstroPi-system "$HOME"/.AstroPi-system
-	fi
-echo "# Check the AstroPi git for update."
-	if ! git -C "$GitDir" pull; then
-		cd "$GitDir" || exit 1
-		git reset --hard
-		echo "$password" | sudo -S chown -R "$USER":"$USER" "$GitDir" | git -C "$GitDir" pull || git reset --hard origin/main
-		git -C "$GitDir" pull || zenity --error --text="<b>Something went wrong. I can't update the repository files.</b>\n\nContact support at\n<b>https://github.com/Andre87osx/AstroPi-system/issues" --width=$W --title="AstroPi - System $AstroPi_v" && exit 1
-	fi
-fi
-echo "# Loading AstroPi - System."
+# =========================================================================
+# Check the AstroPi git for update.
+	(
+	# =================================================================
+	echo "5"
+	echo "# Preparing update"
+	sleep 2s
+        git -C "$WorkDir" pull
+        case $? in
+        0)
+        	true
+		#zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
+        ;;
+        1)
+		#echo "$password" | sudo -S rm -rf "$WorkDir"
+		#cd "$HOME" || exit
+		#git clone https://github.com/Andre87osx/AstroPi-system.git
+		#mv $HOME/AstroPi-system $HOME/.AstroPi-system
+		#git -C "$WorkDir" pull
+		# zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
+		exit 1
+        ;;
+        -1)
+		#echo "$password" | sudo -S rm -rf "$WorkDir"
+		#cd "$HOME" || exit
+		#git clone https://github.com/Andre87osx/AstroPi-system.git
+		#mv $HOME/AstroPi-system $HOME/.AstroPi-system
+		#git -C "$WorkDir" pull
+		# zenity --info --text="All file have been successfully updated" --width=300 --title="AstroPi System"
+		exit 1
+        ;;
+        esac
 
-# I make sure that the scripts are executable
-echo "$password" | sudo -S chmod +x "$GitDir"/Script/*.sh
-) | zenity --progress --title="Loading AstroPi - System $AstroPi_v..." --percentage=1 --pulsate --auto-close --auto-kill --width=$Wprogress
+	# =================================================================
+	echo "75"
+	echo "# Make sure that the script are executable"
+	sleep 2s
+        chmod +x "$WorkDir"/Script/*.sh
+        chmod +x "$WorkDir"/AstroPiSystem/*.sh
+			
+	# =================================================================
+	echo "100"
+	echo "# One meore second"
+        sleep 1s 
+        
+	) | zenity --progress \
+	--title="AstroPi System $ASTROPI_V" \
+	--text="AstroPi System $ASTROPI_V" \
+	--percentage=0 \
+	--auto-close \
+	--width=300 \
+	--auto-kill
 
-# Export the variable to AstroPi.sh script
+# ========================================================================
+
+# I export the password to the script AstroPi.sh
 export password
-export AstroPi_v
-export GitDir
-export WorkDir
-export KStars_v
-export Indi_v
-export W
-export H
-export Wprogress
+"$WorkDir"/Script/AstroPi.sh
 
-# Start AstroPi.sh
-"$GitDir"/Script/AstroPi.sh
-(( $? != 0 )) && zenity --error --text="Something went wrong. Contact support at\n<b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --width=$W --title="AstroPi - System '$AstroPi_v'" && exit 1
+# launch AstroPi.sh
+echo "$password" | sudo -S "$WorkDir"/Script/AstroPi.sh
+(( $? != 0 )) && zenity --error --text="Something went wrong. Contact support at\n<b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --width=300 --title="AstroPi System" && exit 1
 exit 0
