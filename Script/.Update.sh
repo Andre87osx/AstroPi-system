b0ad823b00fcff6458dc390076a490cd0bf844b3
Andrea Aimone <78596979+Andre87osx@users.noreply.github.com>
2021-12-16 19:37:07 +0100

    fix minor typo

--- Script/.Update.sh
+++ Script/.Update.sh
@@ -22,7 +22,6 @@ else
 	WorkDir="$HOME"/.Projects
 fi
 
-
 # Get width of screen and height of screen
 SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
 SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
