e06d7ddc4f25efc32887173b61a65b97abe1d739
Andrea Aimone <78596979+Andre87osx@users.noreply.github.com>
2022-01-30 11:11:54 +0100

    change method update AstroPi System, not use GIT
    [ x ] add new app path
    [ x ] delete git pull
    [ x ] add wget for update script

--- Script/.Update.sh
+++ Script/.Update.sh
@@ -24,7 +24,7 @@ H=$(( SCREEN_HEIGHT / 3 ))
 Wprogress=$(( SCREEN_WIDTH / 5 ))
 
 W_Title="AstroPi System v${AstroPi_v}"
-W_err_generic="Something went wrong...\nContact support at\n<b>https://github.com/Andre87osx/AstroPi-system/issues</b>"
+W_err_generic="<b>Something went wrong...</b>\nContact support at\nhttps://github.com/Andre87osx/AstroPi-system/issues"
 
 # Chk USER and create path
 if [[ -z ${USER} ]] && [[ ${USER} != root ]];
@@ -32,8 +32,9 @@ then
 	zenity --error --text="<b>Run this script as USER not as root</b>\n\nError in AstroPi System" --width=${W} --title="${W_Title}"
 	exit 1
 else
-	GitDir="${HOME}"/.AstroPi-system
+	GitDir="${HOME}"/.AstroPi-system	# Deprecated after v1.5
 	WorkDir="${HOME}"/.Projects
+	AppDir="${HOME}"/.local/share/astropi
 fi
 
 # Sudo password request.
@@ -45,40 +46,44 @@ if [ ${exit_stat} -ne 0 ]; then
 else
 	# User write password and press OK
 	# Makes sure that the user sudo password is correct
-	until [ $"(echo $password | sudo -S echo '' 2>/dev/null)" ]; do
-		zenity --warning --text="<b>The password is incorrect.</b>\n\nTry again" --width=${W} --title="${W_Title}"
+	until [ $"(echo $password | sudo -S echo '' >/dev/null 2>&1)" ]; do
+		zenity --warning --text="<b>The user password is wrong. Try again...</b>\n
+		For issue contact support at\nhttps://github.com/Andre87osx/AstroPi-system/issues" --width=${W} --title="${W_Title}"
 		exit 1
 	done
 fi
 
-# Check and update AstroPi GIT
+# Check AstroPi System
 (
-echo "# Check internet connection first"
-if [ "$(wget -q --spider https://github.com/Andre87osx/AstroPi-system)" ]; then
-	echo "# Check if GIT dir exist"
-	if [ ! -d "${GitDir}" ]; then
-		cd "${HOME}" || exit 1
-		git clone https://github.com/Andre87osx/AstroPi-system.git
-		mv "${HOME}"/AstroPi-system "${GitDir}"
+	if [ ! -d "${AppDir}" ]; then
+		mkdir -p "${AppDir}"
 	fi
-	echo "# Loading AstroPi - System."
-	if [ ! git -C "${GitDir}" pull ]; then
-		cd "${GitDir}" || exit 1
-		git reset --hard
-		echo "$password" | sudo -S chown -R "${USER}":"${USER}" "${GitDir}" | git -C "${GitDir}" pull || git reset --hard origin/main
-		git -C "${GitDir}" pull || zenity --error --text="${W_err_generic}" --width=${W} --title="${W_Title}" && exit 1
+	echo "# Check for internet connection"
+	if [ "$(wget -q --spider https://github.com/Andre87osx/AstroPi-system)" ]; then
+		echo "# AstroPi are connected"
+		if [ ! -f "${AppDir}/.Update.sh" ]; then
+			wget https://github.com/Andre87osx/AstroPi-system/blob/v"${AstroPi_v}"/Script/AstroPi.sh
+		fi
+	else
+		if [ ! -f "${AppDir}/AstroPi.sh" ]; then
+			zenity --warning --text="<b>AstroPi System is not installed correctly.</b>
+			Connect to the internet to be able to download the necessary updates. 
+			\nThe program will be finished. Try again" --width=${W} --title="${W_Title}"
+			exit 1
+		fi
 	fi
-fi
 
 # I make sure that the scripts are executable
-echo "$password" | sudo -S chmod +x -R "${GitDir}"/Script
+echo "$password" | sudo -S chmod +x -R "${AppDir}"/*.sh >/dev/null 2>&1
+echo "$password" | sudo -S chmod +x -R "${AppDir}"/*.py >/dev/null 2>&1
 
-) | zenity --progress --title="Loading AstroPi - System ${AstroPi_v}..." --percentage=1 --pulsate --auto-close --auto-kill --width=${Wprogress}
+) | zenity --progress --title="Loading AstroPi - System v${AstroPi_v}..." --percentage=1 --pulsate --auto-close --auto-kill --width=${Wprogress}
 
 # Export the variable to AstroPi.sh script
 export password
 export AstroPi_v
 export GitDir
+export AppDir
 export WorkDir
 export KStars_v
 export Indi_v
@@ -89,7 +94,7 @@ export W_err_generic
 export W_Title
 
 # Start AstroPi.sh
-if [ "$(bash "${GitDir}"/Script/AstroPi.sh)" ]; then
+if [ "$(bash "${Appdir}"/AstroPi.sh)" ]; then
 	exit 0
 else
 	zenity --error --text="${W_err_generic}" --width=${W} --title="${W_Title}"
