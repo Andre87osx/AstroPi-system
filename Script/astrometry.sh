27f2260ebc8e9eca818c5930283f93946a60a1ec
Andrea Aimone <78596979+Andre87osx@users.noreply.github.com>
2022-02-12 18:14:05 +0100

    rework astrometry.sh for v1.5

--- Script/astrometry.sh
+++ Script/astrometry.sh
@@ -38,118 +38,55 @@
 # Scales 08-19 are named like index-40XX.fits.bz2 and each one covers
 # the whole sky.
 
-(
-    cd ${IndexPath} || exit 1
-    for i in `seq -w 00 47`; do
-	if [ ! -f ${IndexPath}/index-4200-${i}.fits ]; then
-	    Index=("index-4200-${i}.fits")
+IndexPath="${HOME}"/.local/share/kstars/astrometry
+
+cd ${IndexPath} || exit 1
+for y in `seq -w 00 47`; do
+for x in `seq -w 00 04`; do
+	Index=("index-42${x}-${y}.fits")
+	if [ ! -f ${Index} ]; then
         # Download missing Index file
-        wget http://data.astrometry.net/4200/index-4200-${i}.fits
+        ( wget http://data.astrometry.net/4200/index-42${x}-${y}.fits ) 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, Time \3/' | \
+		zenity --progress --title="Downloading ${Index}..." --pulsate --auto-close --auto-kill --width=420
 	else
-		Index=("index-4200-${i}.fits")
-        echo "${Index} found"
+        echo "${Index} found in ${IndexPath}"
 	fi
 done
-
-for i in `seq -w 00 47`; do
-	if [ ! -f ${IndexPath}/index-4201-${i}.fits ]; then
-	    Index=("index-4201-${i}.fits")
-        # Download missing Index file
-        wget --continue http://broiler.astrometry.net/~dstn/4200/index-4201-${i}.fits
-	else
-		Index=("index-4201-${i}.fits")
-        echo "${Index} found"
-	fi
 done
 
-for i in `seq -w 00 47`; do
-	if [ ! -f ${IndexPath}/index-4202-${i}.fits ]; then
-	    Index=("index-4202-${i}.fits")
+for y in `seq -w 00 11`; do
+for x in `seq -w 05 07`; do
+	Index=("index-42${x}-${y}.fits")
+	if [ ! -f ${Index} ]; then
         # Download missing Index file
-        wget --continue http://broiler.astrometry.net/~dstn/4200/index-4202-${i}.fits
+        ( wget http://data.astrometry.net/4200/index-42${x}-${y}.fits ) 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, Time \3/' | \
+		zenity --progress --title="Downloading ${Index}..." --pulsate --auto-close --auto-kill --width=420
 	else
-		Index=("index-4202-${i}.fits")
-        echo "${Index} found"
+        echo "${Index} found in ${IndexPath}"
 	fi
 done
-
-for i in `seq -w 00 47`; do
-	if [ ! -f ${IndexPath}/index-4203-${i}.fits ]; then
-	    Index=("index-4203-${i}.fits")
-        # Download missing Index file
-        wget --continue http://broiler.astrometry.net/~dstn/4200/index-4203-${i}.fits
-	else
-		Index=("index-4203-${i}.fits")
-        echo "${Index} found"
-	fi
 done
 
-for i in `seq -w 00 47`; do
-	if [ ! -f ${IndexPath}/index-4204-${i}.fits ]; then
-	    Index=("index-4204-${i}.fits")
+for x in `seq -w 4208 4219`; do
+	Index=("index-${x}.fits")
+	if [ ! -f ${Index} ]; then
         # Download missing Index file
-        wget --continue http://broiler.astrometry.net/~dstn/4200/index-4204-${i}.fits
+        ( wget http://data.astrometry.net/4200/index-${x}.fits ) 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, Time \3/' | \
+		zenity --progress --title="Downloading ${Index}..." --pulsate --auto-close --auto-kill --width=420
 	else
-		Index=("index-4204-${i}.fits")
-        echo "${Index} found"
-	fi
-done
-
-for i in `seq -w 00 11`; do
-	if [ ! -f ${IndexPath}/index-4205-${i}.fits ]; then
-	    Index=("index-4205-${i}.fits")
-        # Download missing Index file
-        wget --continue http://broiler.astrometry.net/~dstn/4200/index-4205-${i}.fits
-	else
-		Index=("index-4205-${i}.fits")
-        echo "${Index} found"
-	fi
-done
-
-for i in `seq -w 00 11`; do
-	if [ ! -f ${IndexPath}/index-4206-${i}.fits ]; then
-	    Index=("index-4206-${i}.fits")
-        # Download missing Index file
-        wget --continue http://broiler.astrometry.net/~dstn/4200/index-4206-${i}.fits
-	else
-		Index=("index-4206-${i}.fits")
-        echo "${Index} found"
-	fi
-done
-
-for i in `seq -w 00 11`; do
-	if [ ! -f ${IndexPath}/index-4207-${i}.fits ]; then
-	    Index=("index-4207-${i}.fits")
-        # Download missing Index file
-        wget --continue http://broiler.astrometry.net/~dstn/4200/index-4207-${i}.fits
-	else
-		Index=("index-4207-${i}.fits")
-        echo "${Index} found"
-	fi
-done
-
-for i in `seq -w 4208 4219`; do
-	if [ ! -f ${IndexPath}/index-${i}.fits ]; then
-	    Index=("index-${i}.fits")
-        # Download missing Index file
-        wget --continue http://broiler.astrometry.net/~dstn/4200/index-${i}.fits
-	else
-		Index=("index-${i}.fits")
-        echo "${Index} found"
+		echo "${Index} found in ${IndexPath}"
 	fi
 done
 
 # Check Thyco2 Index for Astrometry
 # Tycho-2 catalog; scales 7-19 available, good for images wider than 1 degree.
-for i in `seq -w 4107 4119`; do
-	if [ ! -f ${IndexPath}/index-${i}.fits ]; then
-	    Index=("index-${i}.fits")
+for x in `seq -w 4107 4119`; do
+	Index=("index-${x}.fits")
+	if [ ! -f ${Index} ]; then
         # Download missing Index file
-        wget http://data.astrometry.net/4100/index-${i}.fits
+        ( wget http://data.astrometry.net/4100/index-${x}.fits ) 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, Time \3/' | \
+		zenity --progress --title="Downloading ${Index}..." --pulsate --auto-close --auto-kill --width=420
 	else
-		Index=("index-${i}.fits")
-        echo "${Index} found"
+		echo "${Index} found in ${IndexPath}"
 	fi
 done
-
-) | zenity --progress --title="${W_Title}" --percentage=1 --pulsate --auto-close --auto-kill --width=${Wprogress}
