bcf55ba0efe8903dc8b38fd89b9b2df72e8dcc0b
Andrea Aimone <78596979+Andre87osx@users.noreply.github.com>
2022-02-14 20:20:00 +0100

    rework install loop in install.sh

--- script/install.sh
+++ script/install.sh
@@ -31,12 +31,12 @@ https://github.com/Andre87osx/AstroPi-system/issues"
 #=========================================================================
 
 # Ask super user password.
-ask_pass=$( zenity --password  --width=${W} --title="${W_Title}" )
+ask_pass=$( zenity --password --title="${W_Title}" )
 if [ ${ask_pass} ]; then
 	# User write password and press OK
 	# Makes sure that the sudo user password matches
 	until $( echo "${ask_pass}" | sudo -S echo '' 2>/dev/null ); do
-		zenity --warning --text="<b>WARNING! The user password not matches...</b>
+		zenity --warning --text="<b>WARNING! User password is wrong...</b>
 		\nTry again or sign out" --width=${W} --title="${W_Title}"
 		if password=$( zenity --password  --width=${W} --title="${W_Title}" ); then break; else exit 0; fi
 	done
@@ -75,47 +75,57 @@ while ${connection} ; do
 	zenity --progress --title="Downloading AstroPi v${AstroPi_v}..." --pulsate --auto-close --auto-kill --width=420
 	echo ""
     
-	# Make all script executable
-	for f in ${appDir}/script/*.*; do
-		echo "${ask_pass}" | sudo -S chmod +x "${f}"
-	done
-    
 	# Install all script in default path
-	function install()
-	{
-		for script in "$@"; do
-			exit_stat=1     
-			if [[  -f ${appDir}/script/${script} ]]; then
-				if [[ "${script}" = *.sh ]]; then
-					echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /usr/bin/${script} && exit_stat=0
-					echo "Install ${script} in /usr/bin/ "
-				elif [[ "${script}" = *.desktop ]]; then
-					echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /usr/share/applications/${script} && exit_stat=0
-					echo "Install ${script} in /usr/share/applications/ "
-				elif  [[ "${script}" = panel ]]; then
-					echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} ${HOME}/.config/lxpanel/LXDE-pi/panels/${script} && exit_stat=0
-					echo "Install ${script} in ${HOME}/.config/lxpanel/LXDE-pi/panels/ "
-				elif [[ "${script}" = *.service ]]; then
-					echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /etc/systemd/system/${script} && exit_stat=0
-					echo "Install ${script} in /etc/systemd/system/ "
-				elif [[ "${script}" = autohotspot ]]; then
-					echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /usr/bin/${script} && exit_stat=0
-					echo "Install ${script} in /usr/bin/ "
-				fi 
-				if [ ${exit_stat} -ne 0 ]; then
-					zenity --error --width=${W} --text="Something went wrong <b>installing ${script}...</b>
-					Contact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
-					exit 1
-				fi
-			fi
-		done
-	}
-	install AstroPi.sh kstars.sh AstroPi.desktop kstars.desktop panel autohotspot.service autohotspot
+	cd ${appDir}/script || exit 1
+	for f in ./*.sh; do
+		exit_stat=1
+   		echo ${ask_pass} | sudo -S cp "${appDir}"/script/"${f}" /usr/bin/"${f}" && exit_stat=0
+		echo ${ask_pass} | sudo -S chmod +x /usr/bin/"${f}" && exit_stat=0
+		echo "Install ${f} in	/usr/bin/"
+		if [ ${exit_stat} -ne 0 ]; then
+			zenity --error --width=${W} --text="Something went wrong <b>installing ${f}...</b>
+			\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
+			exit 1
+		fi
+	done
+	for f in ./*.desktop; do
+		exit_stat=1
+   		echo ${ask_pass} | sudo -S cp "${appDir}"/script/"${f}" /usr/share/applications/"${f}" && exit_stat=0
+		echo ${ask_pass} | sudo -S chmod +x /usr/share/applications/"${f}" && exit_stat=0
+		echo "Install ${f} in	/usr/share/applications/"
+		if [ ${exit_stat} -ne 0 ]; then
+			zenity --error --width=${W} --text="Something went wrong <b>installing ${f}...</b>
+			\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
+			exit 1
+		fi
+	done
+	while true; do
+		cd ${appDir}/script || exit 1
+		echo ${ask_pass} | sudo -S cp "${appDir}"/script/autohotspot.service /etc/systemd/system/autohotspot.service
+		(($? != 0)) && zenity --error --width=$W --text="Something went wrong in <b>Updating AstroPi Hotspot.service</b>
+		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="AstroPi System $AstroPi_v" && exit 1
+		echo "Install autohotspot.service in	/etc/systemd/system/"
+		
+		echo ${ask_pass} | sudo -S cp "${appDir}"/script/autohotspot /usr/bin/autohotspot
+		(($? != 0)) && zenity --error --width=$W --text="Something went wrong in <b>Updating AstroPi Hotspot script</b>
+		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="AstroPi System $AstroPi_v" && exit 1
+		echo "Install autohotspot in	/usr/bin/"
+		
+		echo ${ask_pass} | sudo -S cp "${appDir}"/script/panel "${HOME}"/.config/lxpanel/LXDE-pi/panels/panel
+		(($? != 0)) && zenity --error --width=$W --text="Something went wrong in <b>editing lxpanels</b>
+		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="AstroPi System $AstroPi_v" && exit 1
+		echo "Install panel in	${HOME}/.config/lxpanel/LXDE-pi/panels/"
+		
+		echo ${ask_pass} | sudo -S cp "${appDir}"/script/parking.py /usr/bin/parking.py
+		(($? != 0)) && zenity --error --width=$W --text="Something went wrong in <b>Updating parking.py</b>
+		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="AstroPi System $AstroPi_v" && exit 1
+		echo "Install parking.py in	/usr/bin/"
+	done
     
 	# Set default wallpaper
 	pcmanfm --set-wallpaper="${appDir}/include/AstroPi_wallpaper.png"
     
-	# Restart LX for able new change icon and wallpaper
+	# Restart LX for able new settings
 	lxpanelctl restart 
     
 	# Installation is finished
