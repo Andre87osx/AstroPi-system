34e8683f492f568f22cb034e31798e3d61cc6378
Andrea Aimone <78596979+Andre87osx@users.noreply.github.com>
2022-02-17 20:03:20 +0100

    rewok update loop

--- script/install.sh
+++ script/install.sh
@@ -28,6 +28,17 @@ Wprogress=$(( SCREEN_WIDTH / 5 ))
 W_Title="AstroPi System v${AstroPi_v}"
 W_err_generic="<b>Something went wrong...</b>\nContact support at
 https://github.com/Andre87osx/AstroPi-system/issues"
+
+apt_commands=(
+'apt update -y'
+'apt upgrade -y'
+'apt dist-upgrade -y'
+'apt full-upgrade -y'
+'apt autopurge -y'
+'apt autoremove -y'
+'apt autoclean -y'
+)
+
 #=========================================================================
 
 # Ask super user password.
@@ -63,17 +74,17 @@ else
 	fi
 fi
 
-# Update RaspberryPi OS, library, app and AstroPi System
-function sysUpgrade()
+# Prepair fot update system
+function system_pre_update()
 {
-	# Check APT Source stops unwanted updates
+	# Check APT Source and stops unwanted updates
 	sources=/etc/apt/sources.list.d/astroberry.list
 	if [ -f "${sources}" ]; then
-		echo "${ask_pass}" | sudo -S chmod 777 "${sources}"
-		echo -e "# deb https://www.astroberry.io/repo/ buster main" | sudo tee "${sources}"
+		echo ${ask_pass} | sudo -S chmod 777 "${sources}"
+		echo -e "# deb https://www.astroberry.io/repo/ buster main" | sudo tee "${sources}" > dev/null
 		(($? != 0)) && zenity --error --width=${W} --text="Something went wrong in <b>sources.list.d</b>
 		\n.Contact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}" && exit 1
-		echo "${ask_pass}" | sudo -S chmod 644 "${sources}"
+		echo ${ask_pass} | sudo -S chmod 644 "${sources}"
 	fi
 	(
 		echo "# Preparing update"
@@ -87,24 +98,77 @@ function sysUpgrade()
 		indi-full libindi-dev libindi1 indi-bin
 		(($? != 0)) && zenity --error --width=${W} --text="Something went wrong in <b>hold some application</b>
 		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}" && exit 1
-		
-		echo "# Run Linux AstroPi full upgrade..."
-		# Run APT FULL upgrade
-		echo "${ask_pass}" | sudo -S apt update && echo "${ask_pass}" | sudo -S apt -y full-upgrade
-		(($? != 0)) && zenity --error --width=${W} --text="Something went wrong in <b>Updating system AstroPi</b>
-		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}" && exit 1
-
-		# =================================================================
-		echo "# All have done"
-		zenity --info --width=${W} --text="All updates have been successfully installed" --title="${W_Title}"
-
+	
 	) | zenity --progress --title="${W_Title}" --percentage=1 --pulsate --auto-close --auto-kill --width=${Wprogress}
 	exit_stat=$?
 	if [ ${exit_stat} -ne 0 ]; then
-			zenity --error --width=${W} --text="Something went wrong in <b>System Update</b>
-			Contact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
-			exit 1
+		zenity --error --width=${W} --text="Something went wrong in <b>System PRE Update</b>
+		Contact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
+		exit 1
 	fi
+}
+
+# Get full AstoPi System update
+function system_update()
+{
+	for CMD in "${apt_commands[@]}"; do
+		echo "---"
+		echo "Running $CMD"
+		echo ""
+		( $CMD ) | zenity --progress --title="${W_Title}" --percentage=1 --pulsate --auto-close --auto-kill --width=${Wprogress}
+
+		if [ $? -eq 0 ]; then
+			echo "$CMD was successfully run, exit status code: $?"
+			echo ""
+		elif [ $? -ne 0 ]; then
+			echo"ERROR: $CMD was not successful!!! Exit status code $?"
+			zenity --error --width=${W} --text="Something went wrong in <b>System Update $CMD</b>
+			\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
+		fi
+	done	
+	echo "Exit status code:  $?"
+	if [ $? -eq 0 ]; then
+		echo "System successfully updated on $(date)" >> ${appDir}/script/update-log.txt
+	elif [ $? -ne 0 ]; then
+		echo "Error running $CMD on $(date), exit status code: $?" >> ${appDir}/script/update-log.txt
+	fi
+}
+
+# Install all script in default path
+function install()
+{
+	for script in "$@"; do
+		exit_stat=1     
+		if [[  -f ${appDir}/script/"${script}" ]]; then
+			if [[ "${script}" = AstroPi.sh ]]; then
+				echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /usr/bin/${script} && exit_stat=0
+				echo "Install ${script} in /usr/bin/ "
+			elif [[ "${script}" = kstars.sh ]]; then
+				echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /usr/bin/${script} && exit_stat=0
+				echo "Install ${script} in /usr/bin/ "
+			elif [[ "${script}" = AstroPi.desktop ]]; then
+				echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /usr/share/applications/${script} && exit_stat=0
+				echo "Install ${script} in /usr/share/applications/ "
+			elif [[ "${script}" = kstars.desktop ]]; then
+				echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /usr/share/applications/${script} && exit_stat=0
+				echo "Install ${script} in /usr/share/applications/ "
+			elif  [[ "${script}" = panel ]]; then
+				echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} ${HOME}/.config/lxpanel/LXDE-pi/panels/${script} && exit_stat=0
+				echo "Install ${script} in ${HOME}/.config/lxpanel/LXDE-pi/panels/ "
+			elif [[ "${script}" = *.service ]]; then
+				echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /etc/systemd/system/${script} && exit_stat=0
+				echo "Install ${script} in /etc/systemd/system/ "
+			elif [[ "${script}" = autohotspot ]]; then
+				echo ${ask_pass} | sudo -S cp ${appDir}/script/${script} /usr/bin/${script} && exit_stat=0
+				echo "Install ${script} in /usr/bin/ "
+			fi 
+			if [ ${exit_stat} -ne 0 ]; then
+				zenity --error --width=${W} --text="<big>Something went wrong <b>installing ${script}...</b></big>
+				\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
+				exit 1
+			fi
+		fi
+	done
 }
 
 echo "Check for internet connection"
@@ -114,81 +178,49 @@ while ${connection} ; do
 	echo ""
 	echo "Downloading AstroPi v${AstroPi_v}..."
 	echo ""
-	( wget -c https://github.com/Andre87osx/AstroPi-system/archive/refs/tags/v"${AstroPi_v}".tar.gz -O - | \
-	tar --strip-components=1 -xz -C ${appDir} ) 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, Time \3/' | \
+	(
+		wget -c https://github.com/Andre87osx/AstroPi-system/archive/refs/tags/v"${AstroPi_v}".tar.gz -O - | \
+		tar --strip-components=1 -xz -C ${appDir}
+	) 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, Time \3/' | \
 	zenity --progress --title="Downloading AstroPi v${AstroPi_v}..." --pulsate --auto-close --auto-kill --width=420
 	echo ""
     
+	# Make all script executable
+	for f in ${appDir}/script/*.sh; do
+		echo "Make executable ${f} script"
+		echo "${ask_pass}" | sudo -S chmod +x "${f}"
+	done
+	for f in ${appDir}/script/*.py; do
+		echo "Make executable ${f} script"
+		echo "${ask_pass}" | sudo -S chmod +x "${f}"
+	done
+    
 	# Install all script in default path
-	cd ${appDir}/script || exit 1
-	for f in *.sh; do
-		exit_stat=1
-   		echo ${ask_pass} | sudo -S cp "${appDir}"/script/"${f}" /usr/bin/"${f}" && exit_stat=0
-		echo ${ask_pass} | sudo -S chmod +x /usr/bin/"${f}" && exit_stat=0
-		echo "Install ${f} in	/usr/bin/"
-		if [ ${exit_stat} -ne 0 ]; then
-			zenity --error --width=${W} --text="Something went wrong <b>installing ${f}...</b>
-			\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
-			exit 1
-		fi
-	done
-	for f in *.desktop; do
-		exit_stat=1
-   		echo ${ask_pass} | sudo -S cp "${appDir}"/script/"${f}" /usr/share/applications/"${f}" && exit_stat=0
-		echo ${ask_pass} | sudo -S chmod +x /usr/share/applications/"${f}" && exit_stat=0
-		echo "Install ${f} in	/usr/share/applications/"
-		if [ ${exit_stat} -ne 0 ]; then
-			zenity --error --width=${W} --text="Something went wrong <b>installing ${f}...</b>
-			\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="${W_Title}"
-			exit 1
-		fi
-	done
-	while true; do
-		cd ${appDir}/script || exit 1
-		echo ${ask_pass} | sudo -S cp "${appDir}"/script/autohotspot.service /etc/systemd/system/autohotspot.service
-		(($? != 0)) && zenity --error --width=$W --text="Something went wrong in <b>Updating AstroPi Hotspot.service</b>
-		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="AstroPi System $AstroPi_v" && exit 1
-		echo "Install autohotspot.service in	/etc/systemd/system/"
-		
-		echo ${ask_pass} | sudo -S cp "${appDir}"/script/autohotspot /usr/bin/autohotspot
-		(($? != 0)) && zenity --error --width=$W --text="Something went wrong in <b>Updating AstroPi Hotspot script</b>
-		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="AstroPi System $AstroPi_v" && exit 1
-		echo "Install autohotspot in	/usr/bin/"
-		
-		echo ${ask_pass} | sudo -S cp "${appDir}"/script/panel "${HOME}"/.config/lxpanel/LXDE-pi/panels/panel
-		(($? != 0)) && zenity --error --width=$W --text="Something went wrong in <b>editing lxpanels</b>
-		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="AstroPi System $AstroPi_v" && exit 1
-		echo "Install panel in	${HOME}/.config/lxpanel/LXDE-pi/panels/"
-		
-		echo ${ask_pass} | sudo -S cp "${appDir}"/script/parking.py /usr/bin/parking.py
-		(($? != 0)) && zenity --error --width=$W --text="Something went wrong in <b>Updating parking.py</b>
-		\nContact support at <b>https://github.com/Andre87osx/AstroPi-system/issues</b>" --title="AstroPi System $AstroPi_v" && exit 1
-		echo "Install parking.py in	/usr/bin/"
-		
-		# Stop loop
-		break
-	done
-	    
+	install AstroPi.sh kstars.sh AstroPi.desktop kstars.desktop panel autohotspot.service autohotspot
+	
+	# Perform PRE update
+	system_pre_update
+	
+	# Get full AstoPi System update
+	system_update
+	
 	# Set default wallpaper
 	pcmanfm --set-wallpaper="${appDir}/include/AstroPi_wallpaper.png"
-	
-	# Update RaspberryPi OS, library, app and AstroPi System
-	sysUpgrade
     
-	# Restart LX for able new settings
+	# Restart LX for able new change icon and wallpaper
 	lxpanelctl restart
 	
-	# Delete old AstroPi installations and GIT
-	while [ -d ${HOME}/.AstroPi-system ]; do
-		echo ${ask_pass} | sudo -S rm -Rf ${HOME}/.AstroPi-system || exit 1
-	done
+	# Delete old AstroPi installations and GIT	
+	if [ -d ${HOME}/.AstroPi-system ]; then	
+		echo ${ask_pass} | sudo -S rm -Rf ${HOME}/.AstroPi-system || exit 1	
+	fi
     
 	# Installation is finished
 	echo ""
-	echo "The installation of AstroPi v${AstroPi_v} is completed. Launch AstroPi to try it out and upgrade KStars AstroPi"
+	echo "The installation of AstroPi v${AstroPi_v} is completed. Launch AstroPi to try it out"
 	zenity --info --width=${W} --text="<b><big>The installation of AstroPi v${AstroPi_v} is completed.</big>
 	\nLaunch AstroPi to try it out</b>" --title="${W_Title}"
-	
-	# Stop loop
+    
+	# STOP LOOP
 	break
 done
