74b72f011e846ce1aeb244261fe7a1e6059a0e76
Andrea Aimone <andrea.aimone@outlook.com>
2026-07-05 20:40:20 +0200

    chore: update version to 1.8.2 in functions and CMakeLists

--- include/functions.sh
+++ include/functions.sh
@@ -12,9 +12,9 @@
 
 # Create version of AstroPi
 majorRelease=1								# Major Release
-minorRelease=8.1							# Minor Release
+minorRelease=8.2							# Minor Release
 AstroPi_v=${majorRelease}.${minorRelease}	# Actual Stable Release
-KStars_v=3.5.4_v1.8.1						# Based on KDE Kstrs v.3.5.4
+KStars_v=3.5.4_v1.8.2						# Based on KDE Kstrs v.3.5.4
 Indi_v=1.9.7								# Based on INDI 1.9.7 Core
 StellarSolver_v=1.9							# From Rlancaste GitHub
 
